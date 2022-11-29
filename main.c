ba9eb438388c8ac8e3e7c5e570f28007edb045fd
kwit <karol.witusik@gmail.com>
2022-11-29 04:14:31 +0100

    first implementation of RSA algorithm

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "rsa.h"
+
+int main(void) {
+	rsa_structure ctx;
+	// todo random prime numbers are for sure better idea
+	rsa_init(59, 67, &ctx);
+
+	char message[] = "Hello World!";
+	uint32_t encrypted_message[sizeof(message)];
+	char decrypted_message[sizeof(message)];
+
+	for (uint32_t i = 0; i < sizeof(message); i++) {
+		encrypted_message[i] = rsa_encrypt(message[i], rsa_get_public_key(&ctx));
+	}
+
+	for (uint32_t i = 0; i < sizeof(message); i++) {
+		decrypted_message[i] = rsa_decrypt(encrypted_message[i], &ctx);
+	}
+
+	printf("Original message: %s\n", message);
+	printf("Decrypted message: %s\n", decrypted_message);
+}
+
